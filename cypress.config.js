5e4330f1a5d54edb2d8ee11c728e1398ece619ad
leafar-Santos <leafarccomputacao@gmail.com>
2026-05-08 03:40:02 -0300

    Ajuste arquivo de geração de relatório

--- cypress.config.js
+++ cypress.config.js
@@ -18,24 +18,23 @@ module.exports = defineConfig({
 
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.js",
-
     baseUrl: process.env.CYPRESS_BASE_URL || "https://front.serverest.dev",
-
     env: {
       apiUrl: process.env.CYPRESS_API_URL || "https://serverest.dev"
     },
-
-    viewportWidth: 1366,
-    viewportHeight: 768,
-    defaultCommandTimeout: 10000,
-    requestTimeout: 10000,
-    responseTimeout: 10000,
     video: true,
     screenshotOnRunFailure: true,
 
     setupNodeEvents(on, config) {
       allureCypress(on, config, {
-        resultsDir: "allure-results"
+        resultsDir: "allure-results",
+        links: {},
+        categories: [],
+        environmentInfo: {
+          framework: "Cypress",
+          project: "ServeRest E2E Automation",
+          environment: envFile
+        }
       });
 
       return config;
